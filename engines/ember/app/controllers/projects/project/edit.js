232f6337318e0bff6d746b7c63e6c45bbef31abd
Oleg Mak <anslapidus@gmail.com>
2016-01-14 23:46:14 +0200

    Some fixes

--- engines/ember/app/controllers/projects/project/edit.js
+++ engines/ember/app/controllers/projects/project/edit.js
@@ -3,12 +3,19 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   request: Ember.inject.service(),
   session: Ember.inject.service(),
+  projectName: '',
+  projectDesc: '',
 
   actions: {
     save() {
-      this.get('project').save().then(() => {
+      let project = this.store.createRecord('project');
+      project.set('title', this.get('projectName'));
+      project.set('description', this.get('projectDesc'));
+      project.save().then(() => {
         this.transitionToRoute('projects.index');
       });
+      this.set('projectName', '');
+      this.set('projectDesc', '');
     },
 
     addUser: function(){
--- engines/ember/app/routes/projects/project/edit.js
+++ engines/ember/app/routes/projects/project/edit.js
@@ -2,12 +2,6 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
-  setupController(controller) {
-    const project = this.modelFor('projects.project').project;
-
-    controller.set('project', project);
-  },
-
   redirect() {
     const model = this.modelFor('projects.project').project;
 
